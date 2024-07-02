e91d37903aadc3249396aa95f223e103be2a87e6
Sans-a <edgarsantinieto007@gmail.com>
2024-07-02 14:16:12 -0500

    Sans-a.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sans-a.py
@@ -0,0 +1,30 @@
+# https://python.org
+
+# comentario en una linea
+
+"""
+esto tambien
+es un comentario
+en varias lineas
+"""
+
+...
+esto tambien
+es un comentario
+en varias lineas
+...
+
+# la varaible la puedo llamar de cualquier forma siempre y cuando siga la sintaxis por defecto del lenguaje
+A = 1
+
+# el lenguaje utilizado no tiene constantes por lo tanto no se ponen
+
+# variables de tipo de dato primitivo
+tipo_string = "texto"
+tipo_int = 8
+tipo_float = 8.8
+tipo_bool = true
+tipo_bool = false
+
+# para imprimir una terminal de texto
+priint ("hola, pyhhon")
